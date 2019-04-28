76b890b914f62afa1d9393e24aaf9f445d62f97b
Libor Kuchař <kuchalib@fit.cvut.cz>
2019-04-28 16:57:32 +0200

    minor changes of code, added const.h and falling edge detector

---
+++ SW/detektor/const.h
@@ -0,0 +1,36 @@
+#pragma once
+
+
+// pin layout
+#define PB0_PIN 0
+#define PB1_PIN 1
+#define PB2_PIN 2
+#define PB3_PIN 3
+#define PB4_PIN 4
+#define PB5_PIN 5
+#define PB6_PIN 6
+#define PB7_PIN 7
+#define PD0_PIN 8
+#define PD1_PIN 9
+#define PD2_PIN 10
+#define PD3_PIN 11
+#define PD4_PIN 12
+#define PD5_PIN 13
+#define PD6_PIN 14
+#define PD7_PIN 15
+#define PC0_PIN 16
+#define PC1_PIN 17
+#define PC2_PIN 18
+#define PC3_PIN 19
+#define PC4_PIN 20
+#define PC5_PIN 21
+#define PC6_PIN 22
+#define PC7_PIN 23
+#define PA0_PIN 24
+#define PA1_PIN 25
+#define PA2_PIN 26
+#define PA3_PIN 27
+#define PA4_PIN 28
+#define PA5_PIN 29
+#define PA6_PIN 30
+#define PA7_PIN 31
---
+++ SW/detektor/detektor.ino
@@ -0,0 +1,94 @@
+#include "const.h"
+
+int sensorValue = 0;            
+uint16_t range[256]; 
+int LED = 1; 
+int analogInPin = PA0_PIN; 
+int tracePin = PB0_PIN; 
+
+ISR(TIMER1_COMPA_vect)
+{
+  noInterrupts();
+  LED ^= 1; 
+  digitalWrite(PC7_PIN,LED); 
+  interrupts();
+}
+
+void setTimer()
+{
+  //https://sites.google.com/site/qeewiki/books/avr-guide/timers-on-the-atmega328
+  // http://www.instructables.com/id/Arduino-Timer-Interrupts/
+  TCNT1 = 0;
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCCR1B = 0b00001101; // clock / 1024
+  // 1000000 Hz / 2**10 = 976,5625 Hz
+  OCR1A = 977; 
+  TIMSK1 = 0b00000010;
+}
+
+void fallingEdgeDetector(int pin)
+{
+  
+  int states[3][2] = { {0,1},{2,1},{2,2} }; 
+	int state = 0;
+  while(true)
+  {
+    int val = digitalRead(pin); 
+    state = states[state][val]; 
+    if(state == 2)
+      return; 
+  }
+
+}
+
+void setup() {
+  
+  Serial.begin(9600);
+  pinMode(PC7_PIN,OUTPUT); 
+  digitalWrite(PC7_PIN, HIGH);   
+  noInterrupts();
+  setTimer(); 
+  interrupts();
+
+  for (int i = 0; i < 256; i++)
+  {
+    range[i] = 0; 
+  }
+}
+
+void loop() {
+  pinMode(tracePin,OUTPUT);
+  digitalWrite(tracePin, HIGH); 
+  delayMicroseconds(1);
+  digitalWrite(tracePin, LOW); 
+  
+  for(uint16_t i = 0; i < 65000; i++)
+  {
+    pinMode(tracePin, INPUT); 
+    fallingEdgeDetector(tracePin); 
+   
+    sensorValue = analogRead(analogInPin);
+    sensorValue >>= 2; 
+    range[sensorValue]++; 
+  
+    pinMode(tracePin,OUTPUT);
+    digitalWrite(tracePin, HIGH); 
+    delayMicroseconds(1);
+    digitalWrite(tracePin, LOW); 
+
+  }
+
+  for(int i = 0; i < 256; i++)
+  {
+     Serial.print(range[i]);
+     Serial.print(";"); 
+  }
+  Serial.println(""); 
+
+  for (int i = 0; i < 256; i++)
+  {
+    range[i] = 0; 
+  }
+
+}
